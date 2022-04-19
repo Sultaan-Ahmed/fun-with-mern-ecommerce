f36c7aa7c41895cb38c4f276841dbc9ee06116ac
Sultaan-Ahmed <sultanduet2019@gmail.com>
2022-04-19 12:01:28 +0600

    .gitignore is now working

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "../client/build")));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
+});
 module.exports = app;
