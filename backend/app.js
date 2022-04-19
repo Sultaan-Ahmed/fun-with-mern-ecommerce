1771fed2e4468bc64fdeef4173e52b191482cf46
Sultaan-Ahmed <sultanduet2019@gmail.com>
2022-04-19 12:12:03 +0600

    build client

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,11 @@ const path = require("path");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// For netlify deployment configuration
 app.use(express.static(path.join(__dirname, "../client/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
+  res.sendFile(path.resolve(__dirname, "../client//build/index.html"));
 });
 module.exports = app;
